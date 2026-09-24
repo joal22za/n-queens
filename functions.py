36f1ff90870a71122878a62f5233a8f5913a3578
Dampish0 <87875399+Dampish0@users.noreply.github.com>
2026-09-24 11:45:40 +0200

    prepped some funcs

--- functions.py
+++ functions.py
@@ -25,12 +25,12 @@ def checkConflicts(board): # count all conflicts in a board
     diag2 = set()  # row + col
 
     for row, col in enumerate(board):
-        if row - col in diag1:
+        if row - col in diag1: # checks the diagonal from top-left to bottom-right
             conflicts += 1
         else:
             diag1.add(row - col)
 
-        if row + col in diag2:
+        if row + col in diag2: # check the diagonal from top-right to bottomleft
             conflicts += 1
         else:
             diag2.add(row + col)
@@ -38,24 +38,15 @@ def checkConflicts(board): # count all conflicts in a board
 
     return conflicts
 
+def generateBoard(n):
+    import random
+    return [random.randint(0, n - 1) for _ in range(n)]
 
-def next_column(board, row):
-    return board[row].index(1)
+def mutate(board):
+    
+    return False
 
-
-def move(queens, board, row=0):
-    if row == queens:
-        return True
-
-    for col in range(queens):
-        if is_safe(board, row, col):
-            board[row][col] = 1
-
-            if move(queens, board, row + 1):
-                return True
-
-            # This choice failed; clear it and try the next column.
-            board[row][col] = 0
+def printBoard(board):
 
     return False
 
