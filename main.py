ada503bce7100d7fa13c00a78bbb5e162f466c37
Matti Al hanota <alma23lj@student.ju.se>
2026-09-24 13:00:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-<<<<<<< Updated upstream
-import functions as p
+import functions
+import random
 
-while True:
-    # Get the number of queens from the user
-    queens = input("Input the number of queens: ")
-    if queens.isdigit():
-        queens = int(queens)
-    else:
-        print("Please enter a valid number of queens.")
-        continue
+def fit(n, child_count = 10, mutation_prob = 0.1, generations = 100):
+    boards = [functions.generateBoard(n) for _ in range(child_count)]
+    max_fitness = n * (n - 1) // 2
     
-    # Check if the number of queens is less than 4 
-    if queens <= 3:
-        print("There is no solution for a board with less than 4 queens.")  
-        continue
+    for i in range(generations):
+        new_boards = []
+        for j in range(child_count):
+            x = functions.select_parent(boards)
+            y = functions.select_parent(boards)
+            while x == y:
+                y = functions.select_parent(boards)
+            child = functions.crossover(x, y)
+            if mutation_prob > random.random():
+                child = functions.mutate(child)
+                
+            new_boards.append(child)
+            
+        boards = new_boards
+        highest_score = []
+        for board in boards:
+            if functions.fitness(board) > functions.fitness(highest_score):
+                highest_score = board
+        
+        functions.printBoard(highest_score)
+        
+        print("Generation: ", i, "Fitness: ", functions.fitness(highest_score))
+        
+        if functions.fitness(highest_score) == max_fitness:
+            print("Solution found!")
+            break
+            
     
-    # Start with an empty board and place one queen in each row.
-    board = [[0 for _ in range(queens)] for _ in range(queens)]
+    
 
-    if p.move(queens, board):
-        for row in board:
-            print(" ".join(str(cell) for cell in row))
-    else:
-        print("No solution found.")
+if __name__ == "__main__":
+    fit(8, child_count=20, mutation_prob=0.1, generations=100)
